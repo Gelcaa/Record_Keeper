3ebef0c5bdc1722ca725a1e302e454395ec7c1e9
Gelcaa <23angelicaysabelbuan@gmail.com>
2023-04-18 00:01:05 +0800

    setting up click listeners

--- app/src/main/java/com/buanangelica/recordkeeper/RunningFragment.kt
+++ app/src/main/java/com/buanangelica/recordkeeper/RunningFragment.kt
@@ -1,20 +1,54 @@
 package com.buanangelica.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.buanangelica.recordkeeper.databinding.FragmentRunningBinding
 
-class RunningFragment : Fragment(){
+class RunningFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentRunningBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_running, container, false)
+
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+
+        return binding.root
 
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+
+
+    }
+
+    private fun setupClickListeners() {
+        binding.container5km.setOnClickListener { launchRunningRecordScreen() }
+        binding.container10km.setOnClickListener { launchRunningRecordScreen() }
+        binding.containerHalfMarathon.setOnClickListener { launchRunningRecordScreen() }
+        binding.containerMarathon.setOnClickListener { launchRunningRecordScreen() }
+
+
+    }
+
+    private fun launchRunningRecordScreen() {
+
+        //in fragment use "context" instead of "this" which id used in activity
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
